0bd380ffa820c2f514910d0eff4971af3cffa0ba
Lucas Liu <lucasqhliu@gmail.com>
2025-05-09 13:39:02 -0700

    feat:added location on the home page

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,8 +1,11 @@
 import RecipeCardGallery from '../components/recipecardgallery';
+import UserLocation from '../components/UserLocation';
 
 export default function home() {
   return (
     <div className="p-6 space-y-6">
+      <h1 className="text-2xl font-semibold text-gray-800">Welcome back!</h1>
+      <UserLocation />
       <h1 className="text-2xl font-semibold text-gray-800">
         Weekly Featured Recipes
       </h1>
