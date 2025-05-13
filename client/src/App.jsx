873b50ecf0c543f0846a724e96c479b639ed6aea
Lizzhan <118141838+Lizzhan@users.noreply.github.com>
2025-05-13 14:06:28 -0700

    refactored footbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,36 +7,11 @@ import Footbar from "./components/footbar";
 import Profile from './pages/UserProfile';
 import Layout from "./layout/Layout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useNavigate, useLocation } from "react-router-dom";
 
 
 export default function App() {
-  // const [pageStack, setPageStack] = useState(['home']);
-  // // const currentPage = pageStack[pageStack.length - 1];
-
-  // // const navigateTo = (page) => {
-  // //   setPageStack((prev) => [...prev, page]);
-  // // };
-
-  // // const goBack = () => {
-  // //   if (pageStack.length > 1) {
-  // //     setPageStack((prev) => prev.slice(0, -1));
-  // //   }
-  // // };
-
   return (
     <div className="flex flex-col min-h-screen bg-kaidCream">
-
-      {/* <Navbar onNavigate={navigateTo} onBack={goBack} />
-      <main className="flex-grow p-4 pb-24 max-w-3xl mx-auto bg-kaidCream">
-        {currentPage === 'home' && <Home />}
-        {currentPage === 'cookbook' && <Cookbook />}
-        {currentPage === 'fridge' && <Fridge onNavigate={navigateTo} />}
-        {currentPage === 'suggest' && <Suggest />}
-        {currentPage === 'profile' && <Profile />}
-      </main>
-      <Footbar onNavigate={navigateTo} /> */}
-
       <Router>
         <Routes>
           <Route element={<Layout />}>
@@ -45,7 +20,6 @@ export default function App() {
             <Route path="/fridge" element={<Fridge />} />
             {/* <Route path="/suggest" element={<Suggest />} /> */}
             <Route path="/profile" element={<Profile />} />
-
             {/* <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} /> */}
           </Route>
