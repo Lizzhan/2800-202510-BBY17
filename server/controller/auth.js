2adfd3f08cb5300d292926cc40d3e7e3c4f9f7c5
Lizzhan <118141838+Lizzhan@users.noreply.github.com>
2025-05-12 19:53:15 -0700

    added controller, routes, and middleware for authentication//user can now login, logout, register//implemented mysql session

---
+++ server/controller/auth.js
@@ -0,0 +1,34 @@
+import bcrypt from 'bcrypt';
+import db from '../db.js'
+
+export const register = (req, res) => {
+    const salt = bcrypt.genSaltSync(12);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+    const q = "INSERT INTO users (email, password, username) VALUES (?, ?, ?)";
+    const values = [req.body.email, hash, req.body.username];
+    db.query(q, values, (err, data) => {
+        if(err) return res.json(err);
+        res.status(200).json("User Registered");
+    })
+
+};
+
+export const login = (req, res) => {
+    const q = "SELECT * FROM users WHERE username=?";
+    db.query(q, [req.body.username, req.body.password], (err, data) => {
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
+        if(!isPasswordCorrect) return res.status(400).json("WRONG PASSWORD");
+        
+		req.session.authenticated = true;
+		req.session.username = req.body.username;
+		console.log(req.session.username);
+		req.session.cookie.maxAge = 60 * 60 * 1000;
+
+        res.status(200).json("user logged in");
+    })
+};
+
+export const logout = (req, res) => {
+    req.session.destroy();
+    res.status(200).json("user logged out");
+}
