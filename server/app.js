4c1dc79b95dc6e67a34622cba51ddbe5fc56d05b
Lucas Liu <lucasqhliu@gmail.com>
2025-05-13 18:25:03 -0700

    feat:added ai dependencies

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ import MySQLStore from 'express-mysql-session';
 import cors from 'cors'
 import db from './db.js';
 import authRoute from './routes/auth.js'
+const { OpenAI } = require('openai');
 
 dotenv.config({
     path: '../.env'
